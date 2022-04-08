71338584fadaf8a2547c23d1fb38d67ec1b6e131
Atal_Kakar <attal1000@hotmail.com>
2022-04-08 11:21:22 +0200

    changed config file interlization

--- ConcertCasus/src/app/concert/concert.model.ts
+++ ConcertCasus/src/app/concert/concert.model.ts
@@ -1,10 +1,11 @@
 import { Music } from "../music/music.model";
+import { Ticket } from "../ticket/ticket.model";
 
 export class Concert{
     _id: string;
     name: string;
     music: Music;
-    tickets: Ticket;
+    tickets: Array<Ticket>;
     date: Date;
     adres: string;
 
