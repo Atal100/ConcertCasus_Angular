8d87d1977db052a39b38c0c9ce4002c6a4d323f4
Atal_Kakar <attal1000@hotmail.com>
2022-04-08 10:44:09 +0200

    Entities added

--- ConcertCasus/src/app/app.module.ts
+++ ConcertCasus/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { MusicComponent } from './music/music.component';
+import { MartistComponent } from './martist/martist.component';
+import { MconcertComponent } from './mconcert/mconcert.component';
+import { MticketComponent } from './mticket/mticket.component';
+import { MuserComponent } from './muser/muser.component';
+import { UserComponent } from './user/user.component';
+import { ArtistComponent } from './artist/artist.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { ConcertComponent } from './concert/concert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    MusicComponent,
+    MartistComponent,
+    MconcertComponent,
+    MticketComponent,
+    MuserComponent,
+    UserComponent,
+    ArtistComponent,
+    TicketComponent,
+    ConcertComponent
   ],
   imports: [
     BrowserModule,
