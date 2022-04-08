0279407e4ab254f3ce68333adf7499ea58335b63
Atal_Kakar <attal1000@hotmail.com>
2022-04-08 12:16:32 +0200

    Added navbar and containers

--- ConcertCasus/src/app/app-routing.module.ts
+++ ConcertCasus/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { ArtistComponent } from './artist/artist.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MusicComponent } from './music/music.component';
+import { ConcertComponent } from './concert/concert.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'artist', component: ArtistComponent},
+  {path: 'music', component: MusicComponent},
+  {path: 'concert', component: ConcertComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
