ca621ef810a066ff65bbaac8aca8501d213f5da6
AtalKakar <70214281+AtalKakar@users.noreply.github.com>
2022-04-10 12:15:49 +0200

    Added user service

---
+++ ConcertCasus/src/app/user/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from "@angular/core";
+import { Observable, BehaviorSubject, throwError } from "rxjs";
+import { map, tap, retry, catchError } from "rxjs/operators";
+import { User } from "./user.model";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: "root"
+})
+export class UserService {
+  users: User[];
+
+  usersAvailable = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) {
+    console.log("UserService constructed");
+    console.log(`Connected to ${environment.apiUrl}`);
+  }
+
+  public getUsers(): Observable<User[]> {
+    console.log("getUsers");
+    return this.http.get<User[]>(`${environment.apiUrl}/api/users`, {});
+  }
+
+  getUser(id: number) {
+    console.log(`getUser(${id})`);
+    return this.http.get(`${environment.apiUrl}` + id);
+  }
+
+  register(user: User) {
+    console.log("createUser");
+    return this.http
+      .post(`${environment.apiUrl}/api/users/register`, user)
+      .pipe(
+        catchError(this.handleError),
+        tap(data => console.log(data))
+      );
+  }
+
+  updateUser(user: User) {
+    console.log("updateUser");
+
+    return this.http.put(`${environment.apiUrl}/api/users/` + user.id, user);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log("handleError" + error);
+
+    return throwError(error.message || error.error.message);
+  }
+}
+
+export interface ApiResponse {
+  results: any[];
+}
